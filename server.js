10955b27e4b6aaf29bcdd398f543a6be9808ecc4
Aspirin 💆🏽 <Aspirin@Samuel-Aspirins-Mac.local>
2022-01-26 13:24:01 +0100

    starting server 💻

--- server.js
+++ server.js
@@ -26,3 +26,9 @@ app.get("/", (req, res)=>{
     res.render('index');
 });
 
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
